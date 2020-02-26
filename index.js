84b74a6a5c7b48efb4df02cb3eda5cfec761e832
JustinKellogg <jlkellogg18@outlook.com>
2020-02-26 13:49:32 -0700

    added questions.js and linked into index

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
 require("dotenv").config();
-console.log(process.env);
+const questions = require("./utils/questions");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const questions = [
-  {
-    name: "githubUsername",
-    type: "input",
-    message: "What is your Github username?"
-  },
-  {
-    name: "name",
-    type: "input",
-    message: "What is the name of your project?"
-  },
-  {
-    name: "description",
-    type: "input",
-    message: "Please provide a short description of your project."
-  },
-  {
-    name: "install",
-    type: "input",
-    message: "What command does the user need to run to install dependencies?"
-  },
-  {
-    name: "usage",
-    type: "input",
-    message: "What is this project used for?"
-  },
-  {
-    name: "license",
-    type: "list",
-    message: "What kind of license would you like?",
-    choices: ["a", "b", "c", "d"]
-  },
-  {
-    name: "contribution",
-    type: "input",
-    message: "What are the contribution parameters for your project?"
-  },
-  {
-    name: "tests",
-    type: "input",
-    message: "How would a user run tests on your project?"
-  }
-];
+inquirer.prompt(questions);
 
 function writeToFile(fileName, data) {}
 
