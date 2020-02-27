911372e0611d79202380634abbdc783a1500f92d
JustinKellogg <jlkellogg18@outlook.com>
2020-02-26 17:28:30 -0700

    sample md to create template to bring over to js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const questions = require("./utils/questions");
+const api = require("./utils/api");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt(questions);
-
 function writeToFile(fileName, data) {}
 
-function init() {}
+async function init() {
+  const userInfo = await inquirer.prompt(questions);
+
+  const githubData = await api.getUser(userInfo.githubUsername);
+  console.log(githubData);
+}
 
 init();
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,10 @@
+const axios = require("axios");
+
 const api = {
-  getUser(username) {}
+  getUser(username) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+    return axios.get(queryUrl);
+  }
 };
 
 module.exports = api;
