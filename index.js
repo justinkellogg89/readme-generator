e6d5e700dab6a541bacc12635db0ca0901dbc47a
JustinKellogg <jlkellogg18@outlook.com>
2020-02-28 09:50:27 -0700

    wrote init function, added readme template

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const api = require("./utils/api");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-function writeToFile(fileName, data) {}
-
 async function init() {
-  const userInfo = await inquirer.prompt(questions);
+  try {
+    const answers = await inquirer.prompt(questions);
+    const githubData = await api.getUser(userInfo.githubUsername);
+    console.log(githubData);
 
-  const githubData = await api.getUser(userInfo.githubUsername);
-  console.log(githubData);
+    myreadme = generateMD(answers);
+
+    fs.writeFile("readme.md", myreadme, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  } catch (err) {
+    return;
+  }
 }
 
 init();
--- utils/generatemarkdown.js
+++ utils/generatemarkdown.js
@@ -1,7 +1,37 @@
 function generateMarkdown(data) {
   return `
-  # ${data.title}
-  
+# about-me-bootstrap
+
+## This repo contains four different html files. An index.html, an aboutme.html, a portfolio.html, and a contact.html. Each serves as a greeting page, a short summary about me, a collection of my work, and a way to contact me, respectively.
+
+## Installation
+
+The only software requirements are a web browser.
+
+## Technologies Used
+
+HTML, CSS, Bootstrap V.4
+
+## Usage
+
+The usage for this repository is a framework for a website that includes the various sections listed above. This layout can be used for anyone who wants to store their projects in one place, or someone who is looking to move into the development field for potential employers to view.
+
+## Project Status
+
+This project will continually be updated as I complete more projects, or as my about me changes. New projects will be in the portfolio, and aboutme may get updated every once in a while as well.
+
+## Credits
+
+Collaborator(s) - Justin Kellogg  
+Outside Resources Used:  
+Bootstrap - www.getbootstrap.com  
+W3Schools - www.w3schools.com
+
+## License
+
+https://justinkellogg89.github.io/about-me-bootstrap/index.html  
+https://github.com/justinkellogg89/about-me-bootstrap
+
   `;
 }
 
